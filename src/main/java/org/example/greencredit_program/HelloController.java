2f4f6c4b4d201c593549e37a8b1bae7f7165a923
deepthansh-m <deepthanshm@gmail.com>
2024-05-26 18:59:20 +0530

    created

---
+++ src/main/java/org/example/greencredit_program/HelloController.java
@@ -0,0 +1,14 @@
+package org.example.greencredit_program;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
